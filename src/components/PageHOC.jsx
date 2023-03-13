1ec11f7cd7294d99265c7d0a83820d5cfc41ba91
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-14 00:31:46 +0530

    Populate HOC with footer text and heroImg and styling

--- src/components/PageHOC.jsx
+++ src/components/PageHOC.jsx
@@ -19,6 +19,25 @@ const PageHOC = (Component, title, description) => () => {
             navigate("/");
           }}
         />
+
+        <div className={styles.hocBodyWrapper}>
+          <div className="flex flex-row w-full">
+            <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+          </div>
+          <p className={`${styles.normalText} my-10`}>{description}</p>
+
+          <Component />
+        </div>
+
+        <p className={styles.footerText}>Made with 💜 by moonman369</p>
+      </div>
+
+      <div className="flex flex-1">
+        <img
+          src={heroImg}
+          alt="hero-img"
+          className="w-full xl:h-full object-cover"
+        />
       </div>
     </div>
   );
