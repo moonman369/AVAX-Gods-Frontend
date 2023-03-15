fd3474bc0a33d555afb9dff3b09d240fdde14ac0
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-15 23:14:39 +0530

    Add '.' to regex and modify app title

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../styles";
 
-const regex = /^[A-Za-z0-9]+$/;
+const regex = /^[A-Za-z0-9.]+$/;
 
 const CustomInput = ({ label, placeholder, value, handleValueChange }) => {
   return (
