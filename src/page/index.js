1e7ba6a7d1de074b1fddb6e4cc6da0811415c633
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-22 00:21:50 +0530

    Create battleground page -import, export and add to route

--- src/page/index.js
+++ src/page/index.js
@@ -2,5 +2,6 @@ import Home from "./Home";
 import CreateBattle from "./CreateBattle";
 import JoinBattle from "./JoinBattle";
 import Battle from "./Battle";
+import BattleGround from "./BattleGround";
 
-export { Home, CreateBattle, JoinBattle, Battle };
+export { Home, CreateBattle, JoinBattle, Battle, BattleGround };
