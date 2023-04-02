c07ef01f87a52b39eb827503db965c7124287892
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-04-02 23:26:01 +0530

    Automatically navigate to ongoing battle if applicable

--- src/page/CreateBattle.jsx
+++ src/page/CreateBattle.jsx
@@ -11,6 +11,10 @@ const CreateBattle = () => {
   const [waitBattle, setWaitBattle] = useState(false);
 
   useEffect(() => {
+    if (gameData?.activeBattle?.battleStatus === 1) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
+    }
+
     if (gameData.activeBattle?.battleStatus === 0) {
       setWaitBattle(true);
     }
